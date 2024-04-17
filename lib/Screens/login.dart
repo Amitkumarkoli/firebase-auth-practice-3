8411570eae5f36d102e9c417cd4249891093528a
Amitkumarkoli <akdev523@gmail.com>
2024-04-17 23:36:26 +0530

    update login screen

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -21,23 +21,30 @@ class _MyHomePageState extends State<Login> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: email,
-                decoration: const InputDecoration(hintText: 'Enter your email'),
-              ),
-              TextField(
-                controller: password,
-                decoration:
-                    const InputDecoration(hintText: 'Enter your password'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(onPressed: (){}, 
-              child: const Text('Log In')),
-            ],
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Image(image: AssetImage('Assets/images/auth_image.png')),
+                  TextField(
+                  controller: email,
+                  decoration: const InputDecoration(hintText: 'Enter your email'),
+                ),
+                TextField(
+                  controller: password,
+                  decoration:
+                      const InputDecoration(hintText: 'Enter your password'),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(onPressed: (){},
+                style: const ButtonStyle(
+                  fixedSize: MaterialStatePropertyAll(Size(150, 50))
+                ), 
+                child: const Text('Log In')),
+              ],
+            ),
           ),
         ));
   }
