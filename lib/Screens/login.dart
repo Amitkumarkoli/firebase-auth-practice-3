320067e45c07feba177304d822b99a58a364648f
Amitkumarkoli <akdev523@gmail.com>
2024-04-21 12:36:21 +0530

    add android platform and multidexSupport

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -20,30 +20,31 @@ class _MyHomePageState extends State<Login> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Image(image: AssetImage('Assets/images/auth_image.png')),
-                TextField(
-                  controller: email,
-                  decoration:
-                      const InputDecoration(hintText: 'Enter your email'),
-                ),
-                TextField(
-                  controller: password,
-                  decoration:
-                      const InputDecoration(hintText: 'Enter your password'),
-                ),
-                const SizedBox(height: 30),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: const ButtonStyle(
-                        fixedSize: MaterialStatePropertyAll(Size(150, 50))),
-                    child: const Text('Log In')),
-              ],
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  const Image(image: AssetImage('Assets/images/auth_image.png')),
+                  TextField(
+                    controller: email,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter your email'),
+                  ),
+                  TextField(
+                    controller: password,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter your password'),
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: const ButtonStyle(
+                          fixedSize: MaterialStatePropertyAll(Size(150, 50))),
+                      child: const Text('Log In')),
+                ],
+              ),
             ),
           ),
         ));
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+import 'dart:io';
+
 import 'package:firebase_auth_practice3/Screens/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+
+void main() async{
+  
+    WidgetsFlutterBinding.ensureInitialized();
+
+    Platform.isAndroid 
+    ?   await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAudwumS4i45qnnLKR29TY8DfH1zXL65d0', 
+        appId: '1:858795082528:android:1658d1c358821698ec8d45', 
+        messagingSenderId: '858795082528', 
+        projectId: 'fir-auth-practice-3'
+    )) 
+  : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
